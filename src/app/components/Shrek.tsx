1541b4e07f08d03e605da70e7e062bb8eee44836
Magnewei <Magnewei@Icloud.com>
2024-04-22 22:49:18 +0200

    added ing

---
+++ src/app/components/Shrek.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Shrek() {
+  return <Image src="../assets/shrek.jpeg" alt="" />;
+}
---
+++ todo.md
@@ -0,0 +1,9 @@
+1. Add counter
+
+2. Add styling
+
+3. Schedule reset
+
+4. Add kevin knap
+
+5. Deploy
