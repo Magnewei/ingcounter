cd83af4db13388d159c71157e1e0a05084c7dabf
Magnewei <Magnewei@Icloud.com>
2024-04-28 13:00:09 +0200

    Removed unnecessary folders.

--- src/components/Footer.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return <div className="inline-block min-h-20 bg-blue-600 align-bottom"></div>;
-}
--- src/components/Shrek.tsx
+++
@@ -1,5 +0,0 @@
-import Image from "next/image";
-
-export default function Shrek() {
-  return <Image src="../assets/shrek.jpeg" alt="" />;
-}
