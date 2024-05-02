84a3443a0daf34ee102c5309fe93660e69396654
Magnewei <Magnewei@Icloud.com>
2024-05-02 21:34:31 +0200

    add hej

--- README.md
+++ README.md
@@ -4,4 +4,3 @@ TODO:
 
 1. Add WebSocket
 2. Add count reset @00:00.
-3. Fix count state?
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,14 @@ import { Ingle } from "~/components/Ingle";
 import { IngCount } from "../actions/IngleAction";
 
 export default async function HomePage() {
+  const style =
+    "flex h-screen flex-col items-center justify-center gap-2 space-x-2 bg-black text-7xl font-bold text-white";
+
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-2 space-x-2 bg-black text-7xl font-bold text-white">
+    <div className={style}>
       <h1>[ ING COUNTER ]</h1>
       <p>{IngCount()}</p>
+      <p>Hej </p>
       <Ingle />
     </div>
   );
